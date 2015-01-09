99b87b7e25eb5e1a39418de1d0ff0c984ca1de06
micdenny <micdenny@gmail.com>
2015-01-09 22:47:10 +0100

    add .nuspec for nuget packaging

--- Source/TestingCI.WhatTheLibrary/Properties/AssemblyInfo.cs
+++ Source/TestingCI.WhatTheLibrary/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestingCI.WhatTheLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Just a project to test some CI service AppVeyor + Rultor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Denny")]
 [assembly: AssemblyProduct("TestingCI.WhatTheLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
