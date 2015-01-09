b1d3f84447facb40f66e9337b8203904af82e7ee
micdenny <micdenny@gmail.com>
2015-01-09 22:06:09 +0100

    add unit tests

---
+++ Source/TestingCI.Tests/WhatDidYouExpectTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingCI.Tests
+{
+    [TestClass]
+    public class WhatDidYouExpectTests
+    {
+        [TestMethod]
+        public void Really_you_want_to_test_me()
+        {
+        }
+
+        [TestMethod]
+        public void No_way_I_gonna_help_you()
+        {
+        }
+    }
+}
