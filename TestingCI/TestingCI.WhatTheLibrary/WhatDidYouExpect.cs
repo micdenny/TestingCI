8959db9fa7dfcb36f1a9c43969101db0d0cb756a
micdenny <micdenny@gmail.com>
2015-01-09 20:53:28 +0100

    first what the library commit

---
+++ TestingCI/TestingCI.WhatTheLibrary/WhatDidYouExpect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingCI.WhatTheLibrary
+{
+    public class WhatDidYouExpect
+    {
+        public void Really()
+        {
+        }
+
+        public void NoWay()
+        {
+        }
+    }
+}
