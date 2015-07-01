3deb2c8b549c4b9022251974fa9d8ac5a8c8e375
Michael Denny <micdenny@gmail.com>
2015-07-01 13:10:20 +0200

    add multiple target framework

---
+++ src/net35/TestingCI.WhatTheLibrary/WhatDidYouExpect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingCI.WhatTheLibrary
+{
+    public class WhatDidYouExpect
+    {
+        public void Really()
+        {
+        }
+
+        public void NoWay()
+        {
+        }
+    }
+}
